70fa022e55355a224effea9a25d80a8e5fbbb454
ducviet01012001@gmail.com <ducviet01012001@gmail.com>
2022-06-15 02:30:59 +0700

    test

--- WebBanlaptop/Controllers/UserController.cs
+++ WebBanlaptop/Controllers/UserController.cs
@@ -1,69 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using WebBanlaptop.Models;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace WebBanlaptop.Controllers
 {
-    public class UserController : Controller
+    public class UserController : Controller 
     {
-        private static string urlAfterLogin; // lưu lại link đang ở trước khi nhấn đăng nhập
-
-
-        QLBANLAPTOPEntities db = new QLBANLAPTOPEntities();
-
-        #region đăng nhập
-        [HttpGet]
-        public ActionResult Login(string strURL)
+        public static string MD5Hash(string input)
         {
-            // kiểm tra đường dẫn có null không, tránh tình trạng paste trực tiếp đường link vào url sẽ không lưu được trả về trang chủ
-            if (!String.IsNullOrEmpty(strURL))
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
             {
-                urlAfterLogin = strURL;
+                hash.Append(bytes[i].ToString("x2"));
             }
-            else
-            {
-                RedirectToAction("Index", "Home");
-            }
+            return hash.ToString();
+        }
+        QLBANLAPTOPEntities db = new QLBANLAPTOPEntities();
+        // GET: User
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+
             return View();
         }
 
         [HttpPost]
-        public ActionResult Login(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult DangKy(FormCollection f, KHACHHANG kh)
         {
-            var username = collection["username"];
-            var password = collection["password"];
-            var user = db.KHACHHANG.SingleOrDefault(p => p.USERNAME == username);
-            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            var userexist = db.KHACHHANG.Any(x => x.USERNAME == kh.USERNAME);
+            var emailexist = db.KHACHHANG.Any(x => x.EMAIL == kh.EMAIL);
+            var sdtexist = db.KHACHHANG.Any(x => x.SDT == kh.SDT);
+            var cccdexist = db.KHACHHANG.Any(x => x.CCCD == kh.CCCD);
+            if (ModelState.IsValid)
             {
-                ViewData["Error"] = "Vui lòng điền đầy đủ nội dung";
-                return this.Login(urlAfterLogin);
+                if (userexist)
+                {
+                    ModelState.AddModelError("USERNAME", "Tên tài khoản đã tồn tại");
+                    return View(kh);
+                }    
+                if(emailexist)
+                {
+                    ModelState.AddModelError("EMAIL", "Email đã được đăng ký mời nhập emmil khác");
+                    return View(kh);
+                }
+                if (sdtexist)
+                {
+                    ModelState.AddModelError("SDT", "SĐT đã được đăng ký mời nhập SĐT khác");
+                    return View(kh);
+                }
+                if (cccdexist)
+                {
+                    ModelState.AddModelError("CCCD", "CCCD đã được đăng ký mời nhập CCCD khác");
+                    return View(kh);
+                }
+                if(kh.SDT.Length != 10)
+                {
+                    ModelState.AddModelError("SDT", "Phải nhập đủ 10 số");
+                }    
+                if(kh.CCCD.Length != 9 && kh.CCCD.Length != 12)
+                {
+                    ModelState.AddModelError("CCCD", "CCCD hoặc là 9 số hoặc là 12 số");
+                }    
+                else
+                {
+                    string mk = f["PASSWORD"].ToString();
+                    string xnmk = f["NHAPLAIPASSWORD"].ToString();
+                    if (mk == xnmk)
+                    {
+                        string pass = MD5Hash(mk);
+                        kh.PASSWORD = pass;
+                        db.KHACHHANG.Add(kh);
+                        db.SaveChanges();
+                       return RedirectToAction("Index","Home");
+                    }
+                    else
+                    {
+                        ViewBag.thongbao = "mật khẩu không trung khớp !!!!!!";
+                        return View();
+                    }  
+                }
             }
-            else if (user == null)
-            {
-                ViewData["Error"] = "invalid username";
-                return this.Login(urlAfterLogin);
-            }
-            else if (!String.Equals(password, user.PASSWORD))
-            {
-                ViewData["Error"] = "invalid password";
-                return this.Login(urlAfterLogin);
-            }
-            else
-            {
-                Session["User"] = user;
-                return Redirect(urlAfterLogin);
-            }
+            return View();
         }
-        #endregion
-        #region đăng xuất
-        public ActionResult LogOut() // đăng xuất
-        {
-            Session["User"] = null;
-            urlAfterLogin = null;
-            return RedirectToAction("Index", "Home");
-        }
-        #endregion
-    }
+
+    }   
 }
