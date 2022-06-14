71f36417da0eb63219b588a65b25af0cb443c2a3
duyle289 <duyle.2892001@gmail.com>
2022-06-15 00:33:50 +0700

    test 1

---
+++ WebBanlaptop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanlaptop.Models;
+
+namespace WebBanlaptop.Controllers
+{
+    public class HomeController : Controller
+    {
+        QLBANLAPTOPEntities db = new QLBANLAPTOPEntities();
+        public ActionResult Index()
+        {
+            var lstSPNB = db.SANPHAM.Where(n => n.SPNOIBAT == true);
+            ViewBag.lstSPNB = lstSPNB;
+            var lstSPM = db.SANPHAM.Where(n => n.SANPHAMMOI == true);
+            ViewBag.lstSPM = lstSPM;
+            return View();
+        }
+        public ActionResult navbarPartial()
+        {
+            var lstSP = db.SANPHAM;
+            return PartialView(lstSP);
+        }
+        public ActionResult showUser()
+        {
+            return PartialView();
+        }
+
+    }
+}
---
+++ WebBanlaptop/Controllers/UserController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanlaptop.Models;
+namespace WebBanlaptop.Controllers
+{
+    public class UserController : Controller
+    {
+        private static string urlAfterLogin; // lưu lại link đang ở trước khi nhấn đăng nhập
+
+
+        QLBANLAPTOPEntities db = new QLBANLAPTOPEntities();
+
+        #region đăng nhập
+        [HttpGet]
+        public ActionResult Login(string strURL)
+        {
+            // kiểm tra đường dẫn có null không, tránh tình trạng paste trực tiếp đường link vào url sẽ không lưu được trả về trang chủ
+            if (!String.IsNullOrEmpty(strURL))
+            {
+                urlAfterLogin = strURL;
+            }
+            else
+            {
+                RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            var username = collection["username"];
+            var password = collection["password"];
+            var user = db.KHACHHANG.SingleOrDefault(p => p.USERNAME == username);
+            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                ViewData["Error"] = "Vui lòng điền đầy đủ nội dung";
+                return this.Login(urlAfterLogin);
+            }
+            else if (user == null)
+            {
+                ViewData["Error"] = "invalid username";
+                return this.Login(urlAfterLogin);
+            }
+            else if (!String.Equals(password, user.PASSWORD))
+            {
+                ViewData["Error"] = "invalid password";
+                return this.Login(urlAfterLogin);
+            }
+            else
+            {
+                Session["User"] = user;
+                return Redirect(urlAfterLogin);
+            }
+        }
+        #endregion
+        #region đăng xuất
+        public ActionResult LogOut() // đăng xuất
+        {
+            Session["User"] = null;
+            urlAfterLogin = null;
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+    }
+}
